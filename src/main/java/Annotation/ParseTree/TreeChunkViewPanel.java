754e12c3d0a1be285e99487c4d1fb61dbb48e6a2
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-03-06 16:42:27 +0300

    Added codes in TreeChunkViewPanel.java for tree drawing.

--- src/main/java/Annotation/ParseTree/TreeChunkViewPanel.java
+++ src/main/java/Annotation/ParseTree/TreeChunkViewPanel.java
@@ -1,8 +1,13 @@
 package Annotation.ParseTree;
 
+import AnnotatedSentence.LayerNotExistsException;
 import AnnotatedSentence.ViewLayerType;
 import AnnotatedTree.ChunkType;
+import AnnotatedTree.ParseNodeDrawable;
+import AnnotatedTree.WordNotExistsException;
 import DataCollector.ParseTree.TreeEditorPanel;
+
+import java.awt.*;
 
 public class TreeChunkViewPanel extends TreeEditorPanel {
     private ChunkType chunkType;
@@ -36,4 +41,54 @@ public class TreeChunkViewPanel extends TreeEditorPanel {
         updateChunks();
     }
 
+    protected int getStringSize(ParseNodeDrawable parseNode, Graphics g) {
+        int i, stringSize = 0;
+        if (parseNode.numberOfChildren() == 0) {
+            try {
+                for (i = 0; i < parseNode.getLayerInfo().getNumberOfWords(); i++)
+                    if (g.getFontMetrics().stringWidth(parseNode.getLayerInfo().getShallowParseAt(i)) > stringSize){
+                        stringSize = g.getFontMetrics().stringWidth(parseNode.getLayerInfo().getShallowParseAt(i));
+                    }
+            } catch (LayerNotExistsException | WordNotExistsException e) {
+                e.printStackTrace();
+            }
+            return stringSize;
+        } else {
+            return g.getFontMetrics().stringWidth(parseNode.getData().getName());
+        }
+    }
+
+    protected void drawString(ParseNodeDrawable parseNode, Graphics g, int x, int y){
+        int i;
+        if (parseNode.numberOfChildren() == 0){
+            g.drawString(parseNode.getLayerData(ViewLayerType.TURKISH_WORD), x, y);
+            g.setColor(Color.RED);
+            try {
+                for (i = 0; i < parseNode.getLayerInfo().getNumberOfWords(); i++){
+                    try {
+                        y += 20;
+                        g.drawString(parseNode.getLayerInfo().getShallowParseAt(i), x, y);
+                    } catch (LayerNotExistsException | WordNotExistsException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (LayerNotExistsException e) {
+                e.printStackTrace();
+            }
+        } else {
+            g.drawString(parseNode.getData().getName(), x, y);
+        }
+    }
+
+    protected void setArea(ParseNodeDrawable parseNode, int x, int y, int stringSize){
+        if (parseNode.numberOfChildren() == 0){
+            try {
+                parseNode.setArea(x - 5, y - 15, stringSize + 10, 20 * (parseNode.getLayerInfo().getNumberOfWords() + 1));
+            } catch (LayerNotExistsException e) {
+                e.printStackTrace();
+            }
+        } else {
+            parseNode.setArea(x - 5, y - 15, stringSize + 10, 20);
+        }
+    }
 }
