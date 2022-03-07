59465eef001021675060011d2e96e43cf9082d07
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-03-07 13:21:43 +0300

    Updated DataCollector dependency.

--- src/main/java/Annotation/Sentence/SentenceShallowParsePanel.java
+++ src/main/java/Annotation/Sentence/SentenceShallowParsePanel.java
@@ -31,7 +31,7 @@ public class SentenceShallowParsePanel extends SentenceAnnotatorPanel {
 
     @Override
     protected void setBounds() {
-        pane.setBounds(((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().x, ((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().y + 20, 240, (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.4));
+        pane.setBounds(((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().getX(), ((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().getY() + 20, 240, (int) (Toolkit.getDefaultToolkit().getScreenSize().height * 0.4));
     }
 
     @Override
